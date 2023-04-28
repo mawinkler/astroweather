03321f6e551b139fc83d657585bbac9af98c0cf3
mawinkler <winkler.info@icloud.com>
2023-04-28 07:07:40 +0000

    code cleanup

--- custom_components/astroweather/weather.py
+++ custom_components/astroweather/weather.py
@@ -69,9 +69,7 @@ from .entity import AstroWeatherEntity
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities
-) -> None:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities) -> None:
     """Set up the AstroWeather weather platform."""
     _LOGGER.info("Set up AstroWeather weather platform")
 
